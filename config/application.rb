a7d5387cbeff9ee8f29e3d71e36fa020fd1e1dff
Michael Vassilevsky <mvassilevsky@gmail.com>
2019-11-13 13:14:09 -0800

    Establish database connection on console start

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module TheSimpleReader
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Establish database connection on console startup.
+    console do
+      ActiveRecord::Base.connection
+    end
   end
 end
