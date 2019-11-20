8957b6fee7e416594825b3f575e2e48249eb1d05
Michael Vassilevsky <mvassilevsky@gmail.com>
2019-11-20 15:52:50 -0800

    Display links to individual feeds

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,4 +3,10 @@ class FeedsController < ApplicationController
     @feeds = current_user.feeds
     @posts = @current_user.posts.order(posted_at: :desc)
   end
+
+  def show
+    @feed = Feed.find(params[:id])
+    @feeds = current_user.feeds
+    @posts = @feed.posts.order(posted_at: :desc)
+  end
 end
