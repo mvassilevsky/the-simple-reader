e04dc6e8e9b1029a588b89b34536b17d2a38a99c
Michael Vassilevsky <mvassilevsky@gmail.com>
2019-11-21 14:34:44 -0800

    Add links to display bookmarked posts

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,12 +1,20 @@
 class FeedsController < ApplicationController
   def index
     @feeds = current_user.feeds
-    @posts = @current_user.posts.order(posted_at: :desc)
+    if params[:bookmarked]
+      @posts = current_user.bookmarked_posts
+    else
+      @posts = current_user.posts.order(posted_at: :desc)
+    end
   end
 
   def show
     @feed = Feed.find(params[:id])
     @feeds = current_user.feeds
-    @posts = @feed.posts.order(posted_at: :desc)
+    if params[:bookmarked]
+      @posts = current_user.bookmarked_posts.where(feed_id: @feed.id)
+    else
+      @posts = @feed.posts.order(posted_at: :desc)
+    end
   end
 end
