a7aace541f5eab2af00af6de86d5fb85b237d122
Michael Vassilevsky <mvassilevsky@gmail.com>
2020-02-06 16:06:21 -0800

    Add tests for posts

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -21,7 +21,25 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post = posts(:post_1)
+  end
+
+  def test_new_post?
+    duplicate_post = @post.dup
+    assert_not(duplicate_post.new_post?)
+
+    duplicate_post = Post.new(feed_id: @post.feed_id,
+                               posted_at: @post.posted_at)
+    assert_not(duplicate_post.new_post?)
+
+    new_post = Post.new
+    assert(new_post.new_post?)
+  end
+
+  def test_read_by?
+    assert(@post.read_by?(users(:user_1)))
+
+    assert_not(@post.read_by?(users(:user_2)))
+  end
 end
